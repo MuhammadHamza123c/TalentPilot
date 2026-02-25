c99018b690f8e61eab5f14084ac7dd642d0ea77d
Muhammad Hamza <muhammadhamzao241@gmail.com>
2026-02-25 10:05:20 +0500

    Update data_read.py

--- app/agents/src/data_read.py
+++ app/agents/src/data_read.py
@@ -1,24 +1,35 @@
 import json
 import os
 import re
+from dotenv import load_dotenv
+from supabase import create_client
+load_dotenv()
+
+
+
+
+load_dotenv()
+supabase_url=os.getenv('SUPABASE_PROJECT_URL')
+supabase_api_key=os.getenv('SUPABASE_API_KEY')
+supabase=create_client(supabase_url,supabase_api_key)
+
+
+
 #(---------------GET DATA FROM DATA.JSON File---------)
-def get_data(image_name:str):
-    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    file_name = os.path.join(PROJECT_ROOT, "data", "data.json")
-    print(f'File Location of data stored: {file_name}')
+def get_data(image_name: str):
     try:
-            with open(file_name,'r',encoding='utf-8') as r:
+        response = supabase.table('resume_data') \
+            .select('image_describe') \
+            .eq('image_name', image_name) \
+            .execute()
 
-                data=json.load(r)
-                
-            for item in data:
-                if item.get('image_name') == image_name:
-                    return re.sub(r'[^\w\s,.!?@#]','',item.get('image_describe', 'No description found'))
-               
-            return 'Image not Found'
-    except FileNotFoundError:
-         return 'File not found on Server'
-    except PermissionError:
-         return 'You dont have permission to read file'
+        data = response.data
 
+        if not data:
+            return "Image not found"
+
+        return data[0]['image_describe']
+
+    except Exception as e:
+        return f"Error: {e}"
 
