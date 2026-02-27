0702bc156401b0bb0cd3844a60eea3500d9efdf6
Muhammad Hamza <muhammadhamzao241@gmail.com>
2026-02-27 16:55:44 +0500

    Fix condition check for user answer response

--- app/agents/new_resume.py
+++ app/agents/new_resume.py
@@ -73,7 +73,7 @@ def main_resume(user_resume_choice:str,take_text:str=None,image_name:str=None):
     
      User_answer=create_new_resume(image_name=image_name,text=take_text)
      
-     if User_answer.question is None:
+     if User_answer.response is None:
             if user_resume_choice=='1':
                 file_name=build_resume_png(User_answer)
             elif user_resume_choice=='2':
