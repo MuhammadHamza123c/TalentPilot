c2f5fb92a9088b7f48fbb7aa86314f11d609e60b
Muhammad Hamza <muhammadhamzao241@gmail.com>
2026-02-27 12:25:05 +0500

    Update image upload handling for PDF and DOCX

--- app/api/routes/upload_image.py
+++ app/api/routes/upload_image.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 from app.api.src.resume_upload import upload_resume
 from app.agents.extract_text import get_text
-from app.api.src.pdf_doc_text import extract_pdf, extract_docx
+from app.api.src.pdf_doc_text import extract_docx_from_url,extract_pdf_from_url
 
 from supabase import StorageException, SupabaseException
 from groq import APIError, RateLimitError, APIConnectionError, APITimeoutError
@@ -18,16 +18,18 @@ async def upload_resume_route(f1: UploadFile = File(...)):
    
         file_extension = f1.filename.split('.')[-1].lower()
         file_name = f"{uuid4()}.{file_extension}"
+        print(file_name)
 
         content = await f1.read()
 
         file_url = upload_resume(image_name=file_name, image_content=content)
+        print('File Upload hgae')
 
         if file_extension == 'pdf':
-            text = extract_pdf(file_url)
+            text = extract_pdf_from_url(file_url)
 
         elif file_extension == 'docx':
-            text = extract_docx(file_url)
+            text = extract_docx_from_url(file_url)
 
         elif file_extension in ['png', 'jpg', 'jpeg']:
             text = get_text(image_name=file_name, image_url=file_url)
